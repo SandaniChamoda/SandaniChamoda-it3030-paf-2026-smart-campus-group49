8b68e3226fb22b9d4b388295990b0ad510c2d96c
Dilmi Chamya <dilmichamya48@gmail.com>
2026-04-12 00:31:26 +0530

    remove unused dotenv import from SmartcampusApplication and add MethodArgumentNotValidException import in GlobalExceptionHandler

--- backend/smartcampus/src/main/java/com/project/smartcampus/exception/GlobalExceptionHandler.java
+++ backend/smartcampus/src/main/java/com/project/smartcampus/exception/GlobalExceptionHandler.java
@@ -3,6 +3,7 @@ package com.project.smartcampus.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
