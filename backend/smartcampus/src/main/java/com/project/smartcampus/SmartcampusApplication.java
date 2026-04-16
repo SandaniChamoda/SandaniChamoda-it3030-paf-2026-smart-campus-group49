ec2fcd6c41fd89b1746839547340968ba1c87b54
nilumi-d <nilumidakshika5@gmail.com>
2026-04-16 11:14:22 +0530

    fix: remove duplicate Dotenv import

--- backend/smartcampus/src/main/java/com/project/smartcampus/SmartcampusApplication.java
+++ backend/smartcampus/src/main/java/com/project/smartcampus/SmartcampusApplication.java
@@ -4,8 +4,6 @@ import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import io.github.cdimascio.dotenv.Dotenv;
-
 @SpringBootApplication
 public class SmartcampusApplication {
 
